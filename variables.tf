97125f7f988cc967b2d14c6fff6994d5f5be0041
mattisafur <46404761+mattisafur@users.noreply.github.com>
2024-11-09 17:14:20 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+variable "environment_name" {
+  description = "Project environemtn name for resource naming."
+  type        = string
+}
+
+variable "cluster_iam_role_arn" {
+  description = "IAM role ARN for EKS cluster."
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CODR block for VPC."
+  type        = string
+}
+variable "subnet_count" {
+  description = "Number of public and private subnets to create."
+  type        = number
+  validation {
+    condition     = var.subnet_count > 0
+    error_message = "At least one public and private subnet must be created."
+  }
+  #   TODO: add validation that the number does not exceed the number of available AZs.
+}
