3ccddd8bf5849ea26d1174e3c0afea73761dec82
mattisafur <46404761+mattisafur@users.noreply.github.com>
2025-01-09 18:18:06 +0200

    added support for node groups, rewrote

--- variables.tf
+++ variables.tf
@@ -1,45 +1,74 @@
-variable "public_subnet_ids" {
-  description = "Public subnets for EKS"
-  type        = set(string)
-}
-variable "private_subnet_ids" {
-  description = "Private subnets for EKS"
-  type        = set(string)
+variable "cluster_name" {
+  type = string
 }
 
-variable "cluster_name" {
-  description = "Name for the created cluster. Other resources will have their names based on the cluster name."
-  type        = string
+variable "public_subnet_ids" {
+  type    = set(string)
+  default = []
+  # TODO validation: at least one subnet (public or private) is defined
+}
+variable "private_subnet_ids" {
+  type    = set(string)
+  default = []
+  # TODO validation: at least one subnet (public or private) is defined
+}
+
+variable "additional_cluster_security_group_ids" {
+  type    = set(string)
+  default = []
 }
 
 variable "cluster_role_name" {
-  description = "IAM role to be used by the cluster"
-  type = string
+  type    = string
   default = "AmazonEKSAutoClusterRole"
 }
 
+variable "kubernetes_version" {
+  type    = string
+  default = null
+}
+
 variable "support_type" {
-  description = "Kubernetes cluster support type. can be set to EXTENDED or STANDARD."
-  type        = string
-  default     = "EXTENDED"
+  type    = string
+  default = "EXTENDED"
+
   validation {
-    condition     = contains(["EXTENDED", "STANDARD"], var.support_type)
-    error_message = "Invalid value"
+    condition     = contains(["STANDARD", "EXTENDED"], var.support_type)
+    error_message = "Support type must be STANDARD or EXTENDED"
   }
 }
 
 variable "fargate_profiles" {
-  description = "List of fargate profiles to be created."
-  type = list(object({
-    name                    = string
+  type = set(object({
+    name = string
+
     pod_execution_role_name = optional(string)
-    selectors = list(object({
-      namespace = optional(string)
-      labels    = optional(map(string))
+    subnet_ids              = optional(set(string))
+
+    selectors = set(object({
+      namespace = string
+
+      labels = optional(map(string))
     }))
   }))
   default = []
-  # TODO add name uniqueness validation
-  # TODO add validation for selector's contents
-  # TODO require at least one selector for each profile
+  # TODO validation (needed?): make sure name is unique
+  # TODO validation: if subnet_ids not defined, at least one private subnet is define in var.private_subnet_ids
+}
+
+variable "node_groups" {
+  type = set(object({
+    name       = string
+    subnet_ids = set(string)
+
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+
+    node_role_name = optional(string)
+  }))
+  default = []
+  # TODO validation (needed?): make sure name is unique
 }
