a549ebfe1dc8d4bd8e3db5d54a0463d341da7281
mattisafur <46404761+mattisafur@users.noreply.github.com>
2025-01-09 18:49:11 +0200

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "cluster_arn" {
-  value = aws_eks_cluster.cluster.arn
+output "cluster" {
+  value = aws_eks_cluster.cluster
+}
+
+output "fargate_profiles" {
+  value = aws_eks_fargate_profile.fargate_profiles
+}
+
+output "node_groups" {
+  value = aws_eks_node_group.node_groups
 }
